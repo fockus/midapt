7aa1196c3e30b965bbb483906c8e7c0b4d4aad32
Fockus <fockus@gmail.com>
2014-02-16 16:23:37 +0400

    Добавил авторизацию, через gem device

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # before_filter :authenticate_user!
+  before_action :authenticate_user!, only: :token
 
   # GET /users
   # GET /users.json
@@ -71,5 +71,8 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email)
+    end
+
+    def token
     end
 end
