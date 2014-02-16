ae7ba28236d9e02ee989e7f1f1fcfa232b3bb055
Fockus <fockus@gmail.com>
2014-02-16 16:49:20 +0400

    Добавил авторизацию, через gem device

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable, :confirmable, :lockable
+         :confirmable, :lockable
 
 	has_many	:minds
 	has_many	:comments
