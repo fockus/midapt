8d242d837fa672c992dcfd058e5445732c185c7e
Dagmagar <dagmagar@gmail.com>
2014-02-26 17:59:22 +0400

    rspec integration

---
+++ spec/models/mind_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Mind do
+  it '1 + 1 == 2' do
+    (1+1).should == 2
+  end
+end
--- test/models/user_mind_tag_mark_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserMindTagMarkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
