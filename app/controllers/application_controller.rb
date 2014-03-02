4e72adb1d79ec59c021898e2dfe4dd1ea2960b8c
ivan-holmansky <ivan.holmansky@gmail.com>
2014-03-03 01:08:46 +0400

    add cancan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+
+  rescue_from CanCan::AccessDenied do
+    render_403
+  end
+
   #rescue_from CanCan::AccessDenied do |exception|
   #  flash[:error] = "Access denied."
   #  redirect_to root_url
@@ -14,6 +19,24 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit! }
   end
+
+  def render_404
+    respond_to do |format|
+      format.html { render :file => 'public/404', :layout => false, :status => :not_found }
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+    end
+  end
+
+  def render_403
+    respond_to do |format|
+      format.html { render :file => 'public/403', :layout => false, :status => :forbidden }
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+    end
+  end
+
+
 
 end
 
--- app/controllers/minds_controller.rb
+++ app/controllers/minds_controller.rb
@@ -1,4 +1,5 @@
 class MindsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_mind, only: [:show, :edit, :update, :destroy]
 
   # GET /minds
