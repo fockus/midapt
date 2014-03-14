547d82600ccb1298e7b5769a8030f85a7b30a3c6
Fockus <fockus@gmail.com>
2014-03-14 23:42:33 +0400

    эксеперемент с can can

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # Turn on request forgery protection. Bear in mind that only non-GET, HTML/JavaScript requests are checked.
   protect_from_forgery with: :exception 
+  
+  if :devise_controller?
+    before_filter :configure_permitted_parameters
+  else
+    enable_authorization
+  end  
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  #enable_authorization :unless => :devise_controller?
+  #before_filter :configure_permitted_parameters, if: :devise_controller?
 
   #check_authorization # Гарантированная проверка прав Can Can
   #skip_authorization_check :only => [:new, :create]
@@ -12,6 +19,22 @@ class ApplicationController < ActionController::Base
   # Перехватываем исключения CanCan
   rescue_from CanCan::AccessDenied do |exception|
     render_403
+  end
+
+  #It may be a good idea to specify the rescue from action:
+  rescue_from CanCan::Unauthorized do |exception|
+    if current_user.nil?
+      session[:next] = request.fullpath
+      puts session[:next]
+      redirect_to login_url, :alert => "You have to log in to continue."
+    else
+      #render :file => "#{Rails.root}/public/403.html", :status => 403
+      if request.env["HTTP_REFERER"].present?
+        redirect_to :back, :alert => exception.message
+      else
+        redirect_to root_url, :alert => exception.message
+      end
+    end
   end
 
   protected
