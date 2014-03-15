4e4294603340152cc2320739d039c452eba88fa7
ivan-holmansky <ivan.holmansky@gmail.com>
2014-03-16 03:15:28 +0400

    fix stream test. (requirements were changed)

--- spec/models/steam_spec.rb
+++ spec/models/steam_spec.rb
@@ -12,18 +12,18 @@ describe Stream do
   it "validates its name and makes sure that it's consisted of 3-120 symbols" do
     stream_2 = Stream.new(name: '@' * 2 )
     stream_3 = Stream.new(name: '@' * 3 )
-    stream_120 = Stream.new(name: '@' * 120 )
-    stream_121 = Stream.new(name: '@' * 121 )
+    stream_30 = Stream.new(name: '@' * 30 )
+    stream_31 = Stream.new(name: '@' * 31 )
 
 
     stream_2.valid?
     stream_3.valid?
-    stream_120.valid?
-    stream_121.valid?
+    stream_30.valid?
+    stream_31.valid?
 
     stream_2.errors[:name].should_not be_empty
     stream_3.errors[:name].should be_empty
-    stream_120.errors[:name].should be_empty
-    stream_121.errors[:name].should_not be_empty
+    stream_30.errors[:name].should be_empty
+    stream_31.errors[:name].should_not be_empty
   end
 end
