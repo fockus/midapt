8bf585912c70a19c4c9d489ed9eb9449c5e420ed
Dagmagar <dagmagar@gmail.com>
2014-03-23 18:10:36 +0400

    fix index action test for unauthorized user

--- spec/controllers/minds_controller_spec.rb
+++ spec/controllers/minds_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe MindsController do
   describe 'GET index' do
-    xit 'renders the index template' do
+    it 'renders the index template' do
 
       get :index
 
       subject.current_user.should be_nil
-      response.status.should eq 403
+      response.status.should be_in(302, 401) # 302 Moved Temporarily - for http requests and redirect to sing_in page and 401 Unauthorized  - for other request types
     end
 
     it 'renders the index template if user is signed in' do
