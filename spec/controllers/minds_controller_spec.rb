28ebddba815ca8a28db1be3dae859ab81f06c915
Dagmagar <dagmagar@gmail.com>
2014-03-23 17:28:16 +0400

    add checking (un)signed user for index action

--- spec/controllers/minds_controller_spec.rb
+++ spec/controllers/minds_controller_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe MindsController do
   describe 'GET index' do
-    it 'renders the index template' do
+    xit 'renders the index template' do
+
+      get :index
+
+      subject.current_user.should be_nil
+      response.status.should eq 403
+    end
+
+    it 'renders the index template if user is signed in' do
       @user = create(:user)
       sign_in @user
 
       get :index
 
+      subject.current_user.should eq @user
       expect(response).to render_template :index
     end
   end
