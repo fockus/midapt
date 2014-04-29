f5dce11cac2f037974f5057f40f7415417638abd
Dagmagar <dagmagar@gmail.com>
2014-04-29 23:59:14 +0400

    minds routing tests

--- spec/controllers/minds_controller_spec.rb
+++ spec/controllers/minds_controller_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe MindsController do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(:get => '/my/minds').to route_to(:controller => 'minds', :action => 'index')
+    end
+
+    it 'routes to #show' do
+      expect(:get => '/my/minds/1').to route_to(:controller => 'minds', :action => 'show', :id => '1')
+    end
+
+    it 'routes to #new' do
+      expect(:get => '/my/minds/new').to route_to(:controller => 'minds', :action => 'new')
+    end
+
+    it 'routes to #create' do
+      expect(:post => '/my/minds/').to route_to(:controller => 'minds', :action => 'create')
+    end
+
+    it 'routes to #update' do
+      expect(:put => '/my/minds/1').to route_to(:controller => 'minds', :action => 'update', :id => '1')
+    end
+
+    it 'routes to #destroy' do
+      expect(:delete => '/my/minds/1').to route_to(:controller => 'minds', :action => 'destroy', :id => '1')
+    end
+
+  end
   describe 'GET index' do
     it 'renders the index template' do
 
@@ -19,5 +45,8 @@ describe MindsController do
       subject.current_user.should eq @user
       expect(response).to render_template :index
     end
+
+
   end
+
 end
