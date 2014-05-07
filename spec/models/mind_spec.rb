2a3f5247e2d256c6c0533c1d10b4f19af8e0ace9
Dagmagar <dagmagar@gmail.com>
2014-05-07 23:23:28 +0400

    use Forgery in minds tests

--- spec/models/mind_spec.rb
+++ spec/models/mind_spec.rb
@@ -3,48 +3,36 @@ require 'spec_helper'
 describe Mind do
 
   it "validates its title and makes sure that it's empty or it's consisted of 5-100 symbols" do
-    mind_nil = Mind.new(title: nil)
-    mind_4 = Mind.new(title: '@' * 4 )
-    mind_5 = Mind.new(title: '@' * 5 )
-    mind_100 = Mind.new(title: '@' * 100 )
-    mind_101 = Mind.new(title: '@' * 101 )
+    mind_nil_title = Mind.new(title: nil)
+    mind_short_title = Mind.new(title: Forgery(:basic).text(:at_least => 1, :at_most => 4) )
+    mind_valid_title = Mind.new(title: Forgery(:basic).text(:at_least => 5, :at_most => 100 ) )
+    mind_long_title = Mind.new(title: Forgery(:basic).text(:at_least => 101, :at_most => 1000 ) )
 
-    mind_nil.valid?
-    mind_4.valid?
-    mind_5.valid?
-    mind_100.valid?
-    mind_101.valid?
+    mind_nil_title.valid?
+    mind_short_title.valid?
+    mind_valid_title.valid?
+    mind_long_title.valid?
 
-    mind_nil.errors[:title].should be_empty
-    mind_4.errors[:title].should_not be_empty
-    mind_5.errors[:title].should be_empty
-    mind_100.errors[:title].should be_empty
-    mind_101.errors[:title].should_not be_empty
+    mind_nil_title.errors[:title].should be_empty
+    mind_short_title.errors[:title].should_not be_empty
+    mind_valid_title.errors[:title].should be_empty
+    mind_long_title.errors[:title].should_not be_empty
   end
 
-  it "validates its text and makes sure that it isn't nil" do
-    mind_nil = Mind.new(text: nil)
+  it "validates its text and makes sure that it exists and it's consisted of 5-1000 symbols" do
+    mind_nil_text = Mind.new(text: nil)
+    mind_short_text = Mind.new(text: Forgery(:basic).text(:at_least => 1, :at_most => 4) )
+    mind_valid_text = Mind.new(text: Forgery(:basic).text(:at_least => 5, :at_most => 1000) )
+    mind_long_text = Mind.new(text: Forgery(:basic).text(:at_least => 1001, :at_most => 10000000) )
 
-    mind_nil.valid?
+    mind_nil_text.valid?
+    mind_short_text.valid?
+    mind_valid_text.valid?
+    mind_long_text.valid?
 
-    mind_nil.errors[:text].should_not be_empty
-  end
-
-  it "validates its text and makes sure that it's consisted of 5-1000 symbols" do
-    mind_4 = Mind.new(text: '@' * 4 )
-    mind_5 = Mind.new(text: '@' * 5 )
-    mind_1000 = Mind.new(text: '@' * 1000 )
-    mind_1001 = Mind.new(text: '@' * 1001 )
-
-
-    mind_4.valid?
-    mind_5.valid?
-    mind_1000.valid?
-    mind_1001.valid?
-
-    mind_4.errors[:text].should_not be_empty
-    mind_5.errors[:text].should be_empty
-    mind_1000.errors[:text].should be_empty
-    mind_1001.errors[:text].should_not be_empty
+    mind_short_text.errors[:text].should_not be_empty
+    mind_valid_text.errors[:text].should be_empty
+    mind_long_text.errors[:text].should_not be_empty
+    mind_nil_text.errors[:text].should_not be_empty
   end
 end
