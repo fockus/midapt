f46d72aab0a1a789d568a55d3791fa034dc45e1a
Dagmagar <dagmagar@gmail.com>
2014-05-11 17:26:54 +0400

    test showing user's minds

--- spec/controllers/minds_controller_spec.rb
+++ spec/controllers/minds_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-def sign_in_user
-  @user = create(:user)
-  sign_in @user
-end
-
 describe MindsController do
   describe 'routing' do
     it 'routes to #index' do
@@ -32,81 +27,95 @@ describe MindsController do
     end
 
   end
-  describe '#index' do
-    it 'renders the index template' do
+  describe 'actions' do
+    let!(:user) { create(:user) }
+    describe '#index' do
+      context 'when user is not signed in' do
+        it 'renders the index template' do
 
-      get :index
+          get :index
 
-      expect(subject.current_user).to be_nil
-      [302, 401].should include response.status # 302 Moved Temporarily - for http requests and redirect to sing_in page and 401 Unauthorized  - for other request types
-    end
-
-    it 'renders the index template if user is signed in' do
-      sign_in_user
-
-      get :index
-
-      expect(subject.current_user).to eq(@user)
-      expect(response).to render_template :index
-    end
-  end
-
-  describe '#create' do
-    context 'when valid' do
-      let!(:mind) { build(:mind) }
-      before do
-        sign_in_user
-        mind.streams_names = FactoryGirl.generate(:valid_stream_name)
-        post :create, :mind => mind.attributes
+          expect(subject.current_user).to be_nil
+          [302, 401].should include response.status # 302 Moved Temporarily - for http requests and redirect to sing_in page and 401 Unauthorized  - for other request types
+        end
       end
 
-      it { expect(response).to redirect_to(mind_path(1)) }
+      context 'when user is signed in' do
+        let!(:mind1) { create(:mind, user: user) }
+        let!(:mind2) { create(:mind, user: user) }
+        before do
+          sign_in user
+          get :index
+        end
+        it 'should accept signed in user' do
+          expect(subject.current_user).to eq(user)
+        end
 
-      it 'should flash notice' do
-        expect(flash[:notice]).to be
+        it { expect(response).to render_template :index }
+
+        it "should assign user's minds" do
+          expect(assigns(:minds)).to match_array([mind1, mind2])
+        end
       end
     end
 
-    context 'when invalid' do
-      let!(:invalid_mind) { build(:invalid_mind) }
-      before do
-        sign_in_user
-        invalid_mind.streams_names = FactoryGirl.generate(:invalid_stream_name)
-        post :create, :mind => invalid_mind.attributes
+    describe '#create' do
+      context 'when valid' do
+        let!(:mind) { build(:mind) }
+        before do
+          sign_in user
+          mind.streams_names = FactoryGirl.generate(:valid_stream_name)
+          post :create, :mind => mind.attributes
+        end
+
+        it { expect(response).to redirect_to(mind_path(1)) }
+
+        it 'should flash notice' do
+          expect(flash[:notice]).to be
+        end
       end
 
-      it { expect(response).to render_template(:new) }
+      context 'when invalid' do
+        let!(:invalid_mind) { build(:invalid_mind) }
+        before do
+          sign_in user
+          invalid_mind.streams_names = FactoryGirl.generate(:invalid_stream_name)
+          post :create, :mind => invalid_mind.attributes
+        end
+
+        it { expect(response).to render_template(:new) }
+      end
     end
-  end
-  describe '#update' do
-    context 'when valid' do
-      let!(:mind) { create(:mind) }
-      before do
-        sign_in mind.user
+    describe '#update' do
+      context 'when valid' do
+        let!(:mind) { create(:mind) }
+        before do
+          sign_in mind.user
 
-        mind.text = FactoryGirl.generate(:valid_mind_text)
-        mind.streams_names = FactoryGirl.generate(:valid_stream_name)
-        put :update, :id => mind.id, :mind => mind.attributes
+          mind.text = FactoryGirl.generate(:valid_mind_text)
+          mind.streams_names = FactoryGirl.generate(:valid_stream_name)
+          put :update, :id => mind.id, :mind => mind.attributes
+        end
+
+        it { expect(response).to redirect_to(mind_path(mind)) }
+
+        it 'should flash notice' do
+          expect(flash[:notice]).to be
+        end
       end
 
-      it { expect(response).to redirect_to(mind_path(mind))}
+      context 'when invalid' do
+        let!(:mind) { create(:mind) }
+        before do
+          sign_in mind.user
 
-      it 'should flash notice' do
-        expect(flash[:notice]).to be
+          mind.text = FactoryGirl.generate(:invalid_mind_text)
+          mind.streams_names = FactoryGirl.generate(:invalid_stream_name)
+          put :update, :id => mind.id, :mind => mind.attributes
+        end
+
+        it { expect(response).to render_template(:edit) }
       end
-    end
-
-    context 'when invalid' do
-      let!(:mind) { create(:mind) }
-      before do
-        sign_in mind.user
-
-        mind.text = FactoryGirl.generate(:invalid_mind_text)
-        mind.streams_names = FactoryGirl.generate(:invalid_stream_name)
-        put :update, :id => mind.id, :mind => mind.attributes
-      end
-
-      it { expect(response).to render_template(:edit)}
     end
   end
 end
