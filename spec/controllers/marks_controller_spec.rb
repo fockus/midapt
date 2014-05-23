da6ec05c5f01d6a72c96dbce80e74b9df596fd38
Dagmagar <dagmagar@gmail.com>
2014-05-23 16:45:52 +0400

    missing tests

--- spec/controllers/marks_controller_spec.rb
+++ spec/controllers/marks_controller_spec.rb
@@ -18,4 +18,56 @@ describe MarksController do
       expect(:delete => '/minds/1/marks/2').to route_to(:controller => 'marks', :action => 'destroy', :mind_id => '1', :id => '2')
     end
   end
+
+  describe 'actions' do
+    let!(:user) { create(:user) }
+    let!(:mind) { create(:mind) }
+    let!(:mark) { build(:mark, user: user, markable: mind) }
+    describe '#create' do
+      describe 'anonymous' do
+        before do
+          post :create, :mind_id => mind.id, :mark => mark.attributes
+        end
+
+        it('current_user') { expect(subject.current_user).to be_nil }
+
+        it('should forbidden reject') { expect(response.status).to be_in([302, 401]) }
+      end
+
+      describe 'user' do
+        before :each do
+          sign_in user
+        end
+
+        it('current_user') { expect(subject.current_user).to eq(user) }
+
+        context 'when mind does not exist' do
+          before do
+            post :create, :mind_id => 0, :mark => mark.attributes
+          end
+
+          it('should return 404') { expect(response.status).to eq(404) }
+        end
+
+        context 'when mind exists' do
+          before do
+            post :create, :mind_id => mind.id, :mark => mark.attributes
+          end
+
+          it { expect(response).to redirect_to(mind_path(mind)) }
+        end
+
+        context 'when mark exists' do
+          let!(:mark) { create(:mark, user: user, markable: mind) }
+          before do
+            post :create, :mind_id => mind.id, :mark => mark.attributes
+          end
+
+          it { expect(response).to redirect_to(mind_path(mind)) }
+
+          it('should flash alert') { expect(flash[:alert]).to be }
+        end
+      end
+    end
+  end
 end
